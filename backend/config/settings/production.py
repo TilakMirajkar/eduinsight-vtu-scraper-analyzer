3e91cc07bd25a01562eef58bfe72ba17bae36399
TilakMirajkar <teeluck2003@gmail.com>
2026-05-06 14:31:44 +0530

    init: initial project setup and folder structure

---
+++ backend/config/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+DATABASES = {
+    'default': env.db('DATABASE_URL')
+}
+
+CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS')
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
